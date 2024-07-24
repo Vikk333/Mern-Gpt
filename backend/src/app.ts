3ac294f62460fcef2deceecd68e0679222b324cc
Vikk333 <vikashchoudhary7995@gmail.com>
2024-07-24 22:55:10 +0530

    Cookie error

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -8,7 +8,15 @@ config();
 const app = express();
 
 //middlewares
-app.use(cors({ origin: "https://mern-gpt-1-f0iv.onrender.com", credentials: true }));
+const corsOptions = {
+    origin: 'https://mern-gpt-1-f0iv.onrender.com',
+    methods: ['GET', 'POST'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true  // Allow cookies and credentials
+  };
+  
+  app.use(cors(corsOptions));
+// app.use(cors({ origin: "https://mern-gpt-1-f0iv.onrender.com", credentials: true }));
 app.use(express.json());
 app.use(cookieParser(process.env.COOKIE_SECRET));
 
