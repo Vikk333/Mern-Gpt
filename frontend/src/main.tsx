0f6472e0a83eafcc29e0a552648b13cb48db6a26
VIKASH CHOUDHARY <110330223+Vikk333@users.noreply.github.com>
2024-07-24 22:07:37 +0530

    Update main.tsx

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -7,7 +7,7 @@ import { BrowserRouter } from "react-router-dom";
 import { AuthProvider } from "./context/AuthContext.tsx";
 import { Toaster } from "react-hot-toast";
 import axios from "axios";
-axios.defaults.baseURL = "http://mern-gpt-c1s5.onrender.com/api/v1/";
+axios.defaults.baseURL = "https://mern-gpt-c1s5.onrender.com/api/v1/";
 axios.defaults.withCredentials = true;
 const theme = createTheme({
   typography: {
